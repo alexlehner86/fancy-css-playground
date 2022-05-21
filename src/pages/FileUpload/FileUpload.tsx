1d3e7d26dc087bedf7d55a287f3a443076fed774
Alexander Lehner <xander_lehner@gmx.at>
2022-05-21 17:38:25 +0200

    style file upload, show file name

--- src/pages/FileUpload/FileUpload.tsx
+++ src/pages/FileUpload/FileUpload.tsx
@@ -1,21 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './FileUpload.module.css';
 
-const FileUpload: React.FunctionComponent = () => (
-    <div className={styles.page}>
-        <h2>
-            Styled File Upload
-        </h2>
-        <form action="#">
-            <label htmlFor="filepicker" className={styles.filePicker}>
-                <span>PDF hochladen</span>
-                <input id="filepicker" type="file" accept=".pdf" aria-describedby="filename"></input>
-            </label>
-            <span id="filename">beispieldatei.pdf</span>
-        </form>
-        {/* https://scottaohara.github.io/a11y_styled_form_controls/src/file-upload/ */}
-    </div>
-);
+const FileUpload: React.FunctionComponent = () => {
+    const [selectedFile, setSelectedFile] = useState('No file selected');
+    const onFilePickerChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const files = Array.from(event.target.files ?? []);
+        if (files.length > 0) {
+            setSelectedFile(files[0].name);
+        }
+    }
+
+    return (
+        <div className={styles.page}>
+            <h2>
+                Form with Styled File Upload
+            </h2>
+            <form action="#">
+                <div className={styles.filePickerRow}>
+                    <label htmlFor="filepicker" className={styles.filePicker}>
+                        <span>Upload PDF</span>
+                        <input
+                            id="filepicker"
+                            type="file"
+                            accept=".pdf"
+                            aria-describedby="selected-file"
+                            onChange={event => onFilePickerChange(event)}
+                        ></input>
+                    </label>
+                    <p id="selected-file" aria-hidden="true">{selectedFile}</p>
+                    {/* problem with NVDA: https://github.com/nvaccess/nvda/issues/11742 */}
+                </div>
+            </form>
+            <h2>
+                Form with Standard File Upload
+            </h2>
+            <form action="#">
+                <label htmlFor="filepicker-standard">
+                    <span>Upload PDF:&nbsp;</span>
+                    <input id="filepicker-standard" type="file" accept=".pdf"></input>
+                </label>
+            </form>
+        </div>
+    );
+};
 
 export default FileUpload;
