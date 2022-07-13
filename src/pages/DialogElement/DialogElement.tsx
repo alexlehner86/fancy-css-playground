4a925f237dfd738aef3c777005f0e1f7e3cb1380
Alexander Lehner <xander_lehner@gmx.at>
2022-07-13 22:13:54 +0200

    style dialog, close event listener

--- src/pages/DialogElement/DialogElement.tsx
+++ src/pages/DialogElement/DialogElement.tsx
@@ -1,9 +1,20 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import styles from './DialogElement.module.css';
 
 const DialogElement: React.FunctionComponent = () => {
     const dialogRef = useRef<HTMLDialogElement>(null);
+
+    useEffect(() => {
+        // Manually set close event listener for dialog, as React doesn't support this event yet.
+        dialogRef.current?.addEventListener('close', () => {
+            console.warn('closed dialog ' + dialogRef.current?.returnValue);
+        });
+    });
+
+    // https://html.spec.whatwg.org/multipage/interactive-elements.html#the-dialog-element
+    // https://blog.logrocket.com/using-the-dialog-element/
+
     const onOpenDialogClick = () => {
         dialogRef.current?.showModal();
     }
@@ -17,21 +28,28 @@ const DialogElement: React.FunctionComponent = () => {
                 type="button"
                 onClick={onOpenDialogClick}
             >
-                Open Dialog
+                Open Modal Dialog
             </button>
             <dialog ref={dialogRef}>
                 <h3>Personal Information</h3>
+                <p>Please tell me more about you. I want to get to know you better.</p>
                 <form method="dialog">
-                    <label htmlFor="favColor">Favorite color:</label>
-                    <select id="favColor">
-                        <option value="default">Choose...</option>
-                        <option>Red</option>
-                        <option>Blue</option>
-                        <option>Green</option>
-                    </select>
+                    <div className={styles.formField}>
+                        <label htmlFor="favColor">Favorite color:</label>
+                        <select id="favColor">
+                            <option value="default">Choose...</option>
+                            <option>Red</option>
+                            <option>Blue</option>
+                            <option>Green</option>
+                        </select>
+                    </div>
+                    <div className={styles.formField}>
+                        <label htmlFor="favMovie">Favorite movie:</label>
+                        <input id="favMovie" type="text" />
+                    </div>
                     <div className={styles.buttonContainer}>
                         <button value="cancel">Cancel</button>
-                        <button id="confirmBtn" value="default">Confirm</button>
+                        <button className={styles.primaryBtn} value="confirm">Confirm</button>
                     </div>
                 </form>
             </dialog>
